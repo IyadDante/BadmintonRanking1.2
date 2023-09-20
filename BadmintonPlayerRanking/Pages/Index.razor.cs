5421ea6a35370e4d90ee7076bce50419747b3718
Iyad Isleem <issi1991@gmail.com>
2023-09-19 23:47:04 -0400

    Initial Commit

---
+++ BadmintonPlayerRanking/Pages/Index.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BadmintonPlayerRanking.Pages
+{
+    public partial class Index
+    {
+        private string SQLDbConnection = "Code";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
