7b5bfe5bc8bb56cc3570e569ea32cce9daf6d74c
Iyad Isleem <issi1991@gmail.com>
2023-12-08 17:13:25 -0500

    Another Change in the Player Table

--- BadmintonRankingsDB/dbo/Tables/PlayersDetails.sql
+++ BadmintonRankingsDB/dbo/Tables/PlayersDetails.sql
@@ -5,8 +5,6 @@
     [LastName] NVARCHAR(50) NULL, 
     [Club] NVARCHAR(50) NULL, 
     [YearOfBrith] NVARCHAR(50) NULL, 
-    [Gender] NVARCHAR(3) NULL, 
-    [AgeGroup] NVARCHAR(3) NULL, 
-    CONSTRAINT [FK_PlayersDetails_ToClubsDetails] FOREIGN KEY ([Club]) REFERENCES [ClubsDetails]([ID]), 
-    CONSTRAINT [FK_PlayersDetails_ToGenderDetails] FOREIGN KEY ([Gender]) REFERENCES [GenderDetails]([Id])
+    [Gender] NVARCHAR(50) NULL, 
+    [AgeGroupID] NVARCHAR(50) NULL 
 )
