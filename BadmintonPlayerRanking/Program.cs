cd168173dab8dfa41ba3b32cc502a6b5751ad65b
Iyad Isleem <issi1991@gmail.com>
2023-12-18 23:42:25 -0500

    IsStaging

--- BadmintonPlayerRanking/Program.cs
+++ BadmintonPlayerRanking/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsProduction())
+if (!app.Environment.IsStaging())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
